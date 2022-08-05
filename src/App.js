31e3fc3bc9e67c50e05dd4f8866c8d46289ba1b4
Kim Chaeleen <cl27au@gmail.com>
2022-08-05 13:38:34 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Counter from "./Counter";
+//import "./App.css";
+
+import MyHeader from "./MyHeader";
+
+function App() {
+  return (
+    <div>
+      <MyHeader />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
