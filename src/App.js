40f8bbf4cf58761beb2ae74c0c21c56cd410d65f
Kim Chaeleen <cl27au@gmail.com>
2022-08-05 15:24:16 +0900

    props example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
+import Container from "./Container";
 import Counter from "./Counter";
 //import "./App.css";
 
 import MyHeader from "./MyHeader";
 
 function App() {
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    initialValue: 5,
+  };
+
   return (
-    <div>
-      <MyHeader />
-      <Counter />
-    </div>
+    <Container>
+      <div>
+        <MyHeader />
+        <Counter {...counterProps} />
+      </div>
+    </Container>
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
+import OddEvenResult from "./OddEvenResult";
 
-const Counter = () => {
-  // 0에서 출발
-  // 1씩 증가하고
-  // 1씩 감소하는
-  // count 상태
-
-  console.log("counter 호출!");
-
-  const [count, setCount] = useState(0);
+const Counter = ({ initialValue }) => {
+  const [count, setCount] = useState(initialValue);
 
   const onIncrease = () => {
     setCount(count + 1);
@@ -18,28 +12,18 @@ const Counter = () => {
     setCount(count - 1);
   };
 
-  //한 개 이상의 state 가능
-  const [count2, setCount2] = useState(0);
-
-  const onIncrease2 = () => {
-    setCount2(count2 + 1);
-  };
-
-  const onDecrease2 = () => {
-    setCount2(count2 - 1);
-  };
-
   return (
     <div>
       <h2>{count}</h2>
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
-
-      <h2>{count2}</h2>
-      <button onClick={onIncrease2}>+</button>
-      <button onClick={onDecrease2}>-</button>
+      <OddEvenResult count={count} />
     </div>
   );
+};
+
+Counter.defautlProps = {
+  initialValue: 0,
 };
 
 export default Counter;
